8e6952731340b4b70bbc1ec7ffbdbe9c838632eb
Niranjan kumar sharma <niranjan1.sharma@ril.com>
2018-04-23 11:01:52 +0530

    minor changes

--- src/pages/StatusInfo.js
+++ src/pages/StatusInfo.js
@@ -7,35 +7,49 @@ class StatusInfo extends Component {
     constructor() {
         super();
         this.state = {
-            date:"",
-            gameId:""
+            date: "",
+            gameId: ""
         }
     }
 
     componentDidMount() {
         this.props.change("Status Info");
     }
-    getInfo(){
+    getInfo() {
         axios({
             method: 'POST',
             url: "http://127.0.0.1:3001/things/",
-            headers: { "Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
         }).then((response) => {
-            console.log(response,"asdfghjkl;")
+            console.log(response, "asdfghjkl;")
 
         }).catch((err) => {
-            console.log(err,"err")
+            console.log(err, "err")
         })
-        
+
     }
     render() {
         return (
             <div className="App">
-                
-                    Enter Date to get Info:
+
+                {/* Enter Date to get Info:
                     <input type="date" className = "date" onChange={(e) => this.setState({ date: e.target.value })} required></input>
-                    <div className = "button" onClick ={()=>this.getInfo()}>Get Game id's</div>
-                
+                    <div className = "button" onClick ={()=>this.getInfo()}>Get Game id's</div> */}
+                <div id="message">
+                    Message:
+                    
+                </div>
+                <div className="socket-left">
+                    <b>Database type:</b>
+                    <select>
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                    </select>
+
+                </div>
+
             </div>
         );
     }
